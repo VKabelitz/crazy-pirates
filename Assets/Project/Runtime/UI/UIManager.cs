bf1ab1714ac579099e8e44f1382f7b9193d1dc49
Lea <leaolt@uni-koblenz.de>
2025-10-12 00:41:26 +0200

    deleted unused code

--- Assets/Project/Runtime/UI/UIManager.cs
+++ Assets/Project/Runtime/UI/UIManager.cs
@@ -1,14 +1,14 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-using System;
 
 public class UIManager : MonoBehaviour
 {
-
     public static UIManager instance;
     public TextMeshProUGUI sprocketText;
     public Slider healthbar;
+
     //public TextMeshProUGUI healthText;
     float healthProgress = 10;
     int sprockets = 0;
@@ -22,21 +22,10 @@ public class UIManager : MonoBehaviour
     {
         sprocketText.text = sprockets.ToString() + " Sprockets";
         //healthText.text = healthProgress.ToString();
-
     }
 
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
-    public void AddSprocket(int amount)
-    {
-        sprockets += amount;
-        //TODO: not always +1, different points for different enemies?
-        sprocketText.text = sprockets.ToString() + " Sprockets";
-
-    }
     public void UpdateSprockets(int currentSprocketAmount)
     {
         sprocketText.text = currentSprocketAmount.ToString() + " Sprockets";
@@ -47,5 +36,4 @@ public class UIManager : MonoBehaviour
         healthProgress -= damage;
         healthbar.value = healthProgress;
     }
-
 }
