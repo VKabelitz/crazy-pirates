ef6c8bb541f19ccf1f2e166004ef3e51fcc635d4
Lena Ihlo <69467121+Lenivaa@users.noreply.github.com>
2025-10-12 23:36:33 +0200

    sprocket icon change

--- Assets/Project/Runtime/LevelManager.cs
+++ Assets/Project/Runtime/LevelManager.cs
@@ -65,7 +65,7 @@ public class LevelManager : MonoBehaviour
     }
 
     public void StartNextLevel()
-    {   
+    {
         Time.timeScale = 1f;
         int nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
         if (nextIndex >= SceneManager.sceneCountInBuildSettings)
